c0d252179f543c48d483fb3be702a6365626a2ee
ahmed-nagi1 <rangopits5@gmail.com>
2024-09-22 22:48:02 -1000

    create friendship

--- social_media_api/friendship/views.py
+++ social_media_api/friendship/views.py
@@ -1,6 +1,5 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import action, api_view, permission_classes
